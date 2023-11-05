8288aad2d3812358c92e852b2a93b06a01a1df6c
ulas96 <y.ulas.yildiz@gmail.com>
2023-11-05 14:22:17 +0300

    Contract update

--- web3/contracts/ExcelciumSwap.sol
+++ web3/contracts/ExcelciumSwap.sol
@@ -1,148 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract UniswapV2SwapExamples {
-    address private constant UNISWAP_V2_ROUTER =
-    0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-
-    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-
-    IUniswapV2Router private router = IUniswapV2Router(UNISWAP_V2_ROUTER);
-    IERC20 private weth = IERC20(WETH);
-    IERC20 private dai = IERC20(DAI);
-
-    // Swap WETH to DAI
-    function swapSingleHopExactAmountIn(
-        uint amountIn,
-        uint amountOutMin
-    ) external returns (uint amountOut) {
-        weth.transferFrom(msg.sender, address(this), amountIn);
-        weth.approve(address(router), amountIn);
-
-        address[] memory path;
-        path = new address[](2);
-        path[0] = WETH;
-        path[1] = DAI;
-
-        uint[] memory amounts = router.swapExactTokensForTokens(
-            amountIn,
-            amountOutMin,
-            path,
-            msg.sender,
-            block.timestamp
-        );
-
-        // amounts[0] = WETH amount, amounts[1] = DAI amount
-        return amounts[1];
-    }
-
-    // Swap DAI -> WETH -> USDC
-    function swapMultiHopExactAmountIn(
-        uint amountIn,
-        uint amountOutMin
-    ) external returns (uint amountOut) {
-        dai.transferFrom(msg.sender, address(this), amountIn);
-        dai.approve(address(router), amountIn);
-
-        address[] memory path;
-        path = new address[](3);
-        path[0] = DAI;
-        path[1] = WETH;
-        path[2] = USDC;
-
-        uint[] memory amounts = router.swapExactTokensForTokens(
-            amountIn,
-            amountOutMin,
-            path,
-            msg.sender,
-            block.timestamp
-        );
-
-        // amounts[0] = DAI amount
-        // amounts[1] = WETH amount
-        // amounts[2] = USDC amount
-        return amounts[2];
-    }
-
-    // Swap WETH to DAI
-    function swapSingleHopExactAmountOut(
-        uint amountOutDesired,
-        uint amountInMax
-    ) external returns (uint amountOut) {
-        weth.transferFrom(msg.sender, address(this), amountInMax);
-        weth.approve(address(router), amountInMax);
-
-        address[] memory path;
-        path = new address[](2);
-        path[0] = WETH;
-        path[1] = DAI;
-
-        uint[] memory amounts = router.swapTokensForExactTokens(
-            amountOutDesired,
-            amountInMax,
-            path,
-            msg.sender,
-            block.timestamp
-        );
-
-        // Refund WETH to msg.sender
-        if (amounts[0] < amountInMax) {
-            weth.transfer(msg.sender, amountInMax - amounts[0]);
-        }
-
-        return amounts[1];
-    }
-
-    // Swap DAI -> WETH -> USDC
-    function swapMultiHopExactAmountOut(
-        uint amountOutDesired,
-        uint amountInMax
-    ) external returns (uint amountOut) {
-        dai.transferFrom(msg.sender, address(this), amountInMax);
-        dai.approve(address(router), amountInMax);
-
-        address[] memory path;
-        path = new address[](3);
-        path[0] = DAI;
-        path[1] = WETH;
-        path[2] = USDC;
-
-        uint[] memory amounts = router.swapTokensForExactTokens(
-            amountOutDesired,
-            amountInMax,
-            path,
-            msg.sender,
-            block.timestamp
-        );
-
-        // Refund DAI to msg.sender
-        if (amounts[0] < amountInMax) {
-            dai.transfer(msg.sender, amountInMax - amounts[0]);
-        }
-
-        return amounts[2];
-    }
-}
-
-interface IUniswapV2Router {
-    function swapExactTokensForTokens(
-        uint amountIn,
-        uint amountOutMin,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external returns (uint[] memory amounts);
-
-    function swapTokensForExactTokens(
-        uint amountOut,
-        uint amountInMax,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external returns (uint[] memory amounts);
-}
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IERC20 {
     function totalSupply() external view returns (uint);
@@ -165,8 +24,39 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint value);
 }
 
-interface IWETH is IERC20 {
-    function deposit() external payable;
 
-    function withdraw(uint amount) external;
+contract ExcelciumSwap is Ownable {
+
+    uint public fee;
+
+    uint256 public ratio;
+
+    address private tokenAddress = 0x9e6969254D73Eda498375B079D8bE540FB42fea7;
+
+    IERC20 private excelcium = IERC20(tokenAddress);
+
+    function swapETHtoEXC(uint256 amount) external payable {
+        require(msg.value >= amount);
+        uint256 excValue = (ratio/100) * amount * (1 - (fee/100));
+        excelcium.transfer(msg.sender, excValue);
+    }
+
+    function approveEXC(uint256 amount) external {
+        excelcium.approve(address(this), amount);
+    }
+
+    function swapEXCtoETH(uint256 amount) external payable {
+        require(excelcium.transferFrom(msg.sender,address(this),amount));
+        uint256 ethValue = (100/ratio) * amount * (1 - (fee/100));
+        payable(address(this)).transfer(ethValue);
+    }
+
+    constructor(uint _fee, uint256 _ratio) Ownable(msg.sender) {
+        fee = _fee;
+        ratio= _ratio;
+    }
 }
+
+
+
+
